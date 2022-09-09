bb13e0573742d970f6f1bb8152dfc2231362708e
cmstew <cmstew@gmail.com>
2022-09-09 14:58:25 -0600

    Add files via upload

---
+++ elements/search-filter-form.php
@@ -0,0 +1,42 @@
+<?php 
+// element-test.php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class Stu_Element_Search_Filter_Form extends \Bricks\Element {
+	// Element properties
+	public $category     = 'Search & Filter'; // Use predefined element category 'general'
+	public $name         = 'stu-search-filter-form'; // Make sure to prefix your elements
+	public $icon         = 'ti-filter'; // Themify icon font class
+
+	// Return localised element label
+	public function get_label() {
+		return esc_html__( 'Search Form', 'bricks' );
+	}
+
+	public function set_control_groups() {}
+
+	// Set builder controls
+	public function set_controls() {
+		$this->controls['searchAndFilterFormId'] = [
+			'tab'         => 'content',
+			'label'       => esc_html__( 'Select a Search Form to Display', 'bricks' ),
+			'type'        => 'select',
+			'options'     => Stu_Search_Filter_Bricks::get_search_form_options(),
+			'placeholder' => esc_html__( 'None', 'bricks' ),
+		];
+		$this->controls['searchAndFilterFormNote'] = [
+			'tab' => 'content',
+			'content' => esc_html__( 'You must select "Custom" for the "Display Results Method" setting in your Search Form otherwise the filter won\'t work.' , 'bricks' ),
+			'type' => 'info',
+		];
+	}
+
+	// Render element HTML
+	public function render() {
+
+		echo "<div {$this->render_attributes( '_root' )}>";
+		echo do_shortcode('[searchandfilter id="' . $this->settings['searchAndFilterFormId'] . '"]');
+		echo "</div>";
+	}
+}
---
+++ stu-search-filter-bricks.php
@@ -0,0 +1,144 @@
+<?php
+/*
+ * Plugin Name: Search & Filter - Bricks Extension
+ * Description: Makes it easier to use Search & Filter with Bricks
+ * Author: Curtis Stewart
+ * Version: 1.0
+ * License: GPL2
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_action( 'plugins_loaded', array( 'Stu_Search_Filter_Bricks', 'get_instance' ) );
+
+Class Stu_Search_Filter_Bricks {
+
+	protected static $instance = null;
+
+	public static function get_instance() {
+
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct() {
+
+		// List of elements to add controls to
+		$elements = [
+			'container',
+			'div',
+			'block',
+			'accordion',
+			'slider',
+			'posts',
+			'woocommerce-products'
+		];
+
+		// Loop through elements and add controls
+		foreach($elements as $element) {
+			add_filter( 'bricks/elements/' . $element . '/controls', function( $controls ) {
+				return self::get_bricks_controls($controls);
+			} );
+		}
+
+		// Add Search & Filter note to Pagination Element
+		add_filter( 'bricks/elements/pagination/controls', function( $controls ) {
+			$new_controls = [ 'searchAndFilterFormNote' => [
+				'tab' => 'content',
+				'content' => esc_html__( 'Search & Filter Note: If you want to use this with Search & Filter, you must put it inside the same container that you put your query loop.' , 'bricks' ),
+				'type' => 'info',
+			]];
+			// Combining this way ensures the note show up first in Bricks
+			return $new_controls + $controls;
+		} );
+
+		// Inject Search & Filter Id into query loop
+		add_filter( 'bricks/posts/query_vars', function( $query_vars, $settings, $element_id ) {
+			if (isset( $settings['searchAndFilterFormId'] )) {
+				$query_vars['search_filter_id'] = $settings['searchAndFilterFormId'];
+			}
+			return $query_vars;
+		}, 10, 3 );
+
+		// Setup Search & Filter Form Element
+		add_action( 'init', function() {
+			$file = '/elements/search-filter-form.php';
+			$element_name = 'stu-search-filter-form';
+			$class_name = 'Stu_Element_Search_Filter_Form';
+			\Bricks\Elements::register_element( __DIR__ . $file, $element_name, $class_name);
+		}, 11 );
+
+	}
+
+	// This function does the brunt of the work of setting up
+	// the controls for each supported Bricks Element
+	private static function get_bricks_controls($controls) {
+		$ordered_controls = [];
+
+		// Loop over existing controls so we can inject our controls exactly where we want them
+		foreach($controls as $k => $v) {
+			$ordered_controls[$k] = $v;
+
+			// Place after the query control with the
+			// exception of the Product element which we
+			// place after the out of stock control
+			if ($k === 'query' ||
+				$k === 'hideOutOfStock') {
+
+				$ordered_controls['searchAndFilterFormId'] = [
+					'tab'         => 'content',
+					'label'       => esc_html__( 'Link with a Search and Filter Form', 'bricks' ),
+					'type'        => 'select',
+					'options'     => self::get_search_form_options(),
+					'placeholder' => esc_html__( 'None', 'bricks' ),
+				];
+				$ordered_controls['searchAndFilterFormNote'] = [
+					'tab' => 'content',
+					'content' => esc_html__( 'Search & Filter Note: If you have Search & Filter Pro and want ajax to work, you must put your query loop inside a container and then put the container ID in the Search Form options.' , 'bricks' ),
+					'type' => 'info',
+				];
+			}
+		}
+
+		// Only make the hasLoop control required if it exists
+		if ( array_key_exists('hasLoop', $controls) ) {
+			$ordered_controls['searchAndFilterFormId']['required'] = [
+				['hasLoop', '!=', ''],
+			];
+			$ordered_controls['searchAndFilterFormNote']['required'] = [
+				['hasLoop', '!=', ''],
+			];
+		}
+
+		// Only add the controls to the query group if
+		// we're in the product element
+		if ( array_key_exists('hideOutOfStock',$controls) ) {
+			$ordered_controls['searchAndFilterFormId']['group'] = 'query';
+			$ordered_controls['searchAndFilterFormNote']['group'] = 'query';
+		};
+
+		return $ordered_controls;
+	}
+
+	// Method to get an array of Search & Filter Forms
+	public static function get_search_form_options() {
+
+		$custom_posts = new WP_Query('post_type=search-filter-widget&post_status=publish&posts_per_page=-1');
+
+		$search_form_options = [];
+		foreach($custom_posts->posts as $post){
+			$search_form_options[$post->ID] = get_the_title($post->ID);
+		}
+
+		wp_reset_postdata();
+
+		return $search_form_options;
+	}
+}
